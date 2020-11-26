4356ab7d70b42990bfa93992b6472995fe8d53e8
touraye <touraye07@gmail.com>
2020-11-26 12:23:17 +0000

    updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 2ray-odd-even-number.github.oi
+
+
+
+### welcome to odd and even number hacking
+
+You can pass in any number and the program will give your result as **odd** or as **even** a number
---
+++ app.js
@@ -0,0 +1,30 @@
+
+let oddOrEven = (number) =>{
+    if(number === 0){
+        return 'Even'
+    } else if (number === 1){
+        return 'Odd'
+    } else {
+        return oddOrEven(number - 2)
+    }
+}
+
+// console.log(oddOrEven());
+
+
+
+function input() {
+    let myInput = prompt('Type in your number');        
+    let option = oddOrEven(myInput);
+    let myAns = document.createElement('h3');
+    const textAns = document.createTextNode(`${myInput} is an ${option} number`);
+    myAns.setAttribute("id", "input");
+    myAns.appendChild(textAns);
+    document.getElementById('result').appendChild(myAns)
+}
+
+function reset() {
+    let refresh = alert('Tap F5 to refresh your broswer')
+    document.getElementById('result').remove();
+}
+
